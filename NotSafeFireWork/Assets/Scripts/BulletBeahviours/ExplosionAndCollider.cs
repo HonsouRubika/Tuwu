0e27bfc0c53db3be9501a7137f90213027ca2cce
Julien <j.haigron@rubika-edu.com>
2022-04-29 10:30:04 +0200

    en cours

--- NotSafeFireWork/Assets/Scripts/BulletBeahviours/ExplosionAndCollider.cs
+++ NotSafeFireWork/Assets/Scripts/BulletBeahviours/ExplosionAndCollider.cs
@@ -12,18 +12,96 @@ public class ExplosionAndCollider : BaseBulletBehaviour {
 	// You can access this.bullet to get the parent bullet script.
 	// After bullet's death, you can delay this script's death : use this.lifetimeAfterBulletDeath.
 
+	public bool isBulletActive = false;
+
 	public ParticleSystem ps;
+	public float[] timeSincePlayerIn;
+	public float nbSecoundForDamages = 2f;
+	public PlayerController[] pc;
 
 	// Use this for initialization (instead of Start)
 	public override void OnBulletBirth ()
 	{
 		base.OnBulletBirth();
+
+		isBulletActive = true;
+
 		ps = GetComponent<ParticleSystem>();
 		ps.Play();
+
+		//init var
+		pc = new PlayerController[2];
+		timeSincePlayerIn = new float[2];
 	}
-	
-	// Update is (still) called once per frame
-	public override void Update ()
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (isBulletActive && other.tag == "Player")
+        {
+			PlayerController _pc = other.gameObject.GetComponent<PlayerController>();
+			if (_pc.isPlayerA)
+            {
+				pc[0] = _pc;
+				timeSincePlayerIn[0] = 0;
+			}
+            else
+			{
+				pc[1] = _pc;
+				timeSincePlayerIn[1] = 0;
+			}
+        }
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+		if (isBulletActive && other.tag == "Player")
+		{
+			if (pc[0] != null)
+			{
+				timeSincePlayerIn[0] += Time.deltaTime;
+                if (timeSincePlayerIn[0] > nbSecoundForDamages)
+				{
+
+					//TODO: Inflict Damages to player 1
+					Debug.Log("player 1 takes X damages");
+
+					timeSincePlayerIn[0] = 0;
+				}
+			}
+			if (pc[1] != null)
+			{
+				timeSincePlayerIn[1] = Time.deltaTime;
+				if (timeSincePlayerIn[1] > nbSecoundForDamages)
+				{
+
+					//TODO: Inflict Damages to player 2
+					Debug.Log("player 2 takes X damages");
+
+					timeSincePlayerIn[1] = 0;
+				}
+			}
+		}
+	}
+
+    public void OnTriggerExit(Collider other)
+    {
+		if (isBulletActive && other.tag == "Player")
+		{
+			Debug.Log("oui");
+			PlayerController _pc = other.gameObject.GetComponent<PlayerController>();
+			if (_pc.isPlayerA)
+			{
+				pc[0] = null;
+			}
+			else
+			{
+				pc[1] = null;
+			}
+		}
+	}
+
+    // Update is (still) called once per frame
+    public override void Update ()
 	{
 		base.Update();
 
@@ -36,6 +114,7 @@ public class ExplosionAndCollider : BaseBulletBehaviour {
 		base.OnBulletDeath();
 
 		// Your code here
+		isBulletActive = false;
 	}
 
 	// This gets called after the bullet has died, it can be delayed.
